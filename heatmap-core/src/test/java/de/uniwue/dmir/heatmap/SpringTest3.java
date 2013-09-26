c4f62eba441cd9073058f2e977775fa839e632bd
Martin Becker <_martinbeckerweb.de>
2013-09-26 17:58:52 +0200

    * removed check against no XML schema in XML files * some file renaming

--- heatmap-core/src/test/java/de/uniwue/dmir/heatmap/SpringTest3.java
+++ heatmap-core/src/test/java/de/uniwue/dmir/heatmap/SpringTest3.java
@@ -37,7 +37,7 @@ public class SpringTest3 {
 		
 		ClassPathXmlApplicationContext appContext = 
 				new ClassPathXmlApplicationContext(
-						"spring/defaultApplicationContext.xml");
+						"spring/example/applicationContext.xml");
 		
 		IHeatmap heatmap = 
 				appContext.getBean(IHeatmap.class);
