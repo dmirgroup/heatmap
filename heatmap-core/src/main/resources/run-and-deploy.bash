f122297e5ec814a1b6d9125010d6be3fbced71de
Martin Becker <_martinbeckerweb.de>
2013-10-07 20:36:00 +0200

    * restructured examples and corresponding scripts

--- heatmap-core/src/main/resources/run-and-deploy.bash
+++
@@ -1,3 +0,0 @@
-SCRIPT_FOLDER=/home/everyaware/heatmap-processor
-bash $SCRIPT_FOLDER/run.bash "$@"
-bash $SCRIPT_FOLDER/deploy.bash "$@"
--- heatmap-core/src/main/resources/run.bash
+++ heatmap-core/src/main/resources/run.bash
@@ -2,14 +2,16 @@
 
 # settings
 DATE_LOG="date.log"
-WORK_DIR="/home/everyaware/heatmap-processor"
 
 # variables
-config_file=settings.xml
-last_date=`tail -n 1 $WORK_DIR/$DATE_LOG`
+work_dir="/home/everyaware/heatmap-processor/workspace/basic"
+config_file="/home/everyaware/heatmap-processor/config/basic/config.xml"
+last_date=`tail -n 1 $work_dir/$DATE_LOG`
+active_profiles="minmax"
+jar_dir="/home/everyaware/heatmap-processor"
 
 # parse options
-while getopts "c:m:" OPTION
+while getopts "c:m:w:a:j:" OPTION
 do
   case $OPTION in
     c)
@@ -17,6 +19,15 @@ do
       ;;
     m)
       last_date=$OPTARG
+      ;;
+    w)
+      work_dir=$OPTARG
+      ;;
+    a)
+      active_profiles=$OPTARG
+      ;;
+    j)
+      jar_dir=$OPTARG
       ;;
   esac
 done
@@ -31,26 +42,28 @@ echo "current date: $current_date"
 
 # append current date to date log
 
-echo $current_date >> $WORK_DIR/$DATE_LOG
+echo $current_date >> $work_dir/$DATE_LOG
 
 # compile command
 
-log_file=`echo $current_date | sed "s/ /_/g" | sed "s/:/-/g"`
+log_file=$work_dir/logs/`echo $current_date | sed "s/ /_/g" | sed "s/:/-/g"`.log
 
 command='
 nice -19 
-flock --timeout=16 '$WORK_DIR'/lock 
+flock --timeout=16 '$work_dir'/lock 
 java 
 -Xmx16g 
 -XX:-UseGCOverheadLimit 
--Dspring.profiles.active="minmax"
+-Dspring.profiles.active="'$active_profiles'"
 -Dmin="'$last_date'" 
 -Dmax="'$current_date'" 
--jar heatmap-core-0.0.1-SNAPSHOT.jar '$config_file'
-> '$WORK_DIR'/logs/'$log_file'.log 2>&1'
+-Dworkdir="file:'$work_dir'" 
+-jar '$jar_dir'/heatmap-core-0.0.1-SNAPSHOT.jar "file:'$config_file'"
+> '$log_file' 2>&1'
 
 # run command
 
 echo "COMMAND:"
 echo $command
 eval $command
+
