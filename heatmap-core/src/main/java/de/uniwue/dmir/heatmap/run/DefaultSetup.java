8e971a6e0f55a817ef0d1cc47ed4c5710ab159ff
Martin Becker <becker@informatik.uni-wuerzburg.de>
2014-09-30 14:33:18 +0200

    * added commandline parser

--- heatmap-core/src/main/java/de/uniwue/dmir/heatmap/run/DefaultSetup.java
+++ heatmap-core/src/main/java/de/uniwue/dmir/heatmap/run/DefaultSetup.java
@@ -5,6 +5,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
+import lombok.Data;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
 import de.uniwue.dmir.heatmap.HeatmapSettings;
 import de.uniwue.dmir.heatmap.IFilter;
 import de.uniwue.dmir.heatmap.IHeatmap;
@@ -43,10 +49,52 @@ import de.uniwue.dmir.heatmap.util.mapper.IMapper;
 
 public class DefaultSetup {
 	
-	public static void main(String[] args) throws IOException {
-		test(new File("src/test/resources/lonlat2.txt"), ",", false, "out");
+	@Data
+	public static class Settings {
+		
+		@Option(name = "-csv", usage = "CSV file to read (format: lon,lat,value)")
+		private File csvFile;
+		
+		@Option(
+				name = "-csvsep", 
+				aliases = {"-csvSeparator"}, 
+				usage = "Cseparator to split lines in CSV file")
+		private String csvSeparator;
+		
+		@Option(
+				name = "-csvskip", 
+				aliases = {"-csvSkipFirstLine"}, 
+				usage = "whether to skip the first line of the CSV file")
+		private boolean csvSkipFirstLine;
+		
+		@Option(name = "-out", usage = "the output folder")
+		private File outputFolder;
+		
+		
 	}
 	
+	public static void main(String[] args) throws IOException {
+		
+		Settings settings = new Settings();
+		CmdLineParser parser = new CmdLineParser(settings);
+		
+		try {
+			parser.parseArgument(args);
+		} catch(CmdLineException e ) {
+			System.err.println(e.getMessage());
+			System.err.println("java -jar myprogram.jar [options...] arguments...");
+			parser.printUsage(System.err);
+			return;
+		}
+		
+		test(
+				settings.getCsvFile(),
+				settings.getCsvSeparator(),
+				settings.isCsvSkipFirstLine(),
+				settings.getOutputFolder().getAbsolutePath());
+	}
+	
+
 	public static void test(File file, String separator, boolean skipFirstLine, String outputParentFolder) throws IOException {
 		
 		// heatmap settings
